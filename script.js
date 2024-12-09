8193373ad6804e09a70a3dce287ec3a43b6adcd8
cordz-del <raarongraham@gmail.com>
2024-12-09 10:23:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function redirectToChatbot() {
     // Redirect to the desired website for now
-    window.location.href = "https://amietech-chatbot-platform.com";
+    window.location.href = "https://cordz-del.github.io/Amie-Chatbot-Frontend/";
 }
